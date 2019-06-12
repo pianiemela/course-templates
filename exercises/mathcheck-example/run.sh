4b43e49572a86757a12218ebfb51d5af5bb20d1b
Santtu Söderholm <santtu.soderholm@tut.fi>
2019-06-12 13:33:31 +0300

    Added a deprecated method of calculating points to mathcheck-example. This will be improved in the near future.

--- exercises/mathcheck-example/run.sh
+++ exercises/mathcheck-example/run.sh
@@ -39,7 +39,7 @@ sed -i 's/&lt;/</g' mc_input
 #capture pre cat mc_input # debug
 
 # ajetaan MC antamalla inputiksi mc_input ja tallentamalla stdout mc_outputiin
-#cat mc_input | mathcheck.out > mc_output
+cat mc_input | mathcheck.out > mc_output
 #capture echo "<br><br>mc_output<br>" # debug
 #capture pre cat mc_output # debug
 
@@ -47,6 +47,32 @@ sed -i 's/&lt;/</g' mc_input
 # "capture" etc description in https://github.com/apluslms/grading-base
 
 capture mathcheck.out < mc_input # in /usr/local/bin
+
+# annetaan pisteitä sen mukaan, oliko mc tyytyväinen
+# huomaa, että plussa skaalaa pisteet. jos plussassa tehtävän maksimipisteiksi on
+# merkitty 50 ja tässä annetaan 1/2, opiskelija saa 25 pst. jos tässä annetaan 2/3,
+# opiskelija saa 33 pst jne.
+
+
+if grep -q "\!points\! 1" mc_output;
+then
+
+  echo "2/2" > /feedback/points
+  echo '<br><br><div class="alert alert-success">Jipii, oikein meni! :)</div>' >> /feedback/out
+
+elif grep -q "class=warn" mc_output;
+then
+
+  echo "1/2" > /feedback/points
+  echo '<br><br><div class="alert alert-warning">Annoit turhan hankalan vastauksen! Voi vipstaakki! Voisikohan vastauksen esittää sievemmin?</div>' >> /feedback/out
+
+else
+
+  echo "0/2" > /feedback/points
+  echo '<br><br><div class="alert alert-danger">Sait nolla pistettä. Yritä uudelleen.</div>' >> /feedback/out
+
+fi
+
 
 err-to-out
 grade
