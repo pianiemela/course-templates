a3b2e453f2d71ed0c89d90312cc14a11f766ebe4
Pia Niemelä <niemelap@tie-lukioplus.rd.tuni.fi>
2019-07-17 14:23:04 +0300

    valtterille

--- docker-compile.sh
+++ docker-compile.sh
@@ -3,6 +3,6 @@
 docker run --rm \
   -v $(pwd):/compile \
   -u $(id -u):$(id -g) \
-  -e "STATIC_CONTENT_HOST=http://localhost:8080/static/default" \
+  -e "STATIC_CONTENT_HOST=https://tie-lukioplus.rd.tuni.fi/grader/static/santtufork/" \
   apluslms/compile-rst:1.6 \
   make touchrst html
